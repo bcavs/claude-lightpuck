9e379716ae21d84f513619c52593f610130a5e57
Ben Cavenagh <cavenagh.ben@gmail.com>
2026-03-30 00:34:11 -0500

    update pin

--- tools/puck_test.py
+++ tools/puck_test.py
@@ -2,7 +2,7 @@ import board
 import neopixel
 import time
 
-pixels = neopixel.NeoPixel(board.D18, 24, brightness=0.2, auto_write=True)
+pixels = neopixel.NeoPixel(board.D10, 24, brightness=0.2, auto_write=True)
 
 pixels.fill((255, 0, 0))  # RED
 time.sleep(10)
